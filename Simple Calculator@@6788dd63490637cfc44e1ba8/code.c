e902f891ce78569446a92f2455388b4b3b61269b
Lakshya-sketch <134377132+Lakshya-sketch@users.noreply.github.com>
2025-02-05 22:22:31 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -24,7 +24,7 @@ int main() {
             printf("%d", num1 / num2);
             break;
         default:
-            printf("Error: Invalid operator");
+            printf("error");
     }
     
     return 0;
