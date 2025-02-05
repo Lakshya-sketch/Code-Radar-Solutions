d1a83a32e3f95cb1d17bae8b848c98c40ca0f65f
Lakshya-sketch <134377132+Lakshya-sketch@users.noreply.github.com>
2025-02-05 14:43:33 +0530

    Solution to Print an Alphabet Right-Angled Triangle

--- Print an Alphabet Right-Angled Triangle@@67877e1818d53eb724f435c5/code.c
+++ Print an Alphabet Right-Angled Triangle@@67877e1818d53eb724f435c5/code.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
-
-
-
 int main() {
-   int a ;
+   int a;
    scanf("%d",&a);
-   for ( int i = 0 ; i < a ; i ++){
-    for ( char chr  = "A" ; chr < "A" ; chr ++){
+   for ( int i = 0 ; i <= a ; i ++){
+    for ( char chr  = "A" ; chr < "A" + i; chr ++){
         print("%c",chr );
     }
     printf("\n");
