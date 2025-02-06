74fb7222575dce79b80882ca9894d84b053c8be2
Lakshya-sketch <134377132+Lakshya-sketch@users.noreply.github.com>
2025-02-06 20:30:50 +0530

    Solution to Find the Second Largest Element in an Array

--- Find the Second Largest Element in an Array@@676c02002dbc8c3e7a280b72/code.c
+++ Find the Second Largest Element in an Array@@676c02002dbc8c3e7a280b72/code.c
@@ -1,47 +1,16 @@
 #include <stdio.h>
-
-void bubbleSort(int arr[], int n) {
-    for (int i = 0; i < n-1; i++) {
-        for (int j = 0; j < n-i-1; j++) {
-            if (arr[j] > arr[j+1]) {
-                int temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
-            }
+int main(){
+    int a;
+    scanf("%d",&a);
+    int arr[a];
+    for( int i - 0 ; i < a; i++){
+        scanf("%d",&a);
+    }
+    for( int i = 0 ; i < a ; i ++ ){
+        if( arr[i+1] > arr[i]){
+            arr[i+1] = arr[i];
         }
     }
-}
-
-int main() {
-    int n;
-    scanf("%d", &n);
-
-    if (n < 2) {
-        printf("-1");
-        return 0;
-    }
-
-    int arr[n];
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &arr[i]);
-    }
-
-    bubbleSort(arr, n);
-
-    int unique[n], uniqueCount = 0;
-    unique[uniqueCount++] = arr[0];
-
-    for (int i = 1; i < n; i++) {
-        if (arr[i] != arr[i - 1]) {
-            unique[uniqueCount++] = arr[i];
-        }
-    }
-
-    if (uniqueCount < 2) {
-        printf("-1");  
-    } else {
-        printf("%d", unique[uniqueCount - 2]); 
-    }
-
+    printf("%d",arr[a-2]) 
     return 0;
 }
