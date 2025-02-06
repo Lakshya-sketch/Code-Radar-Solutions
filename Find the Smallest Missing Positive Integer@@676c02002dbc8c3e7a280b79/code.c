e591894518b18e19639f6e676d6b26d2ba3a1a53
Lakshya-sketch <134377132+Lakshya-sketch@users.noreply.github.com>
2025-02-06 20:15:44 +0530

    Solution to Find the Smallest Missing Positive Integer

--- Find the Smallest Missing Positive Integer@@676c02002dbc8c3e7a280b79/code.c
+++ Find the Smallest Missing Positive Integer@@676c02002dbc8c3e7a280b79/code.c
@@ -2,17 +2,17 @@
 
 int main() {
     int a;
+    scanf("%d", &a);
     int arr[a];
-    scanf("%d",&a);    
-    for ( int i = 0 ; i < a; i++){
-        scanf("%d",&arr[i]);
+    for (int i = 0; i < a; i++){
+        scanf("%d", &arr[i]);
     }
-    int sum1 = 0;
-    int sum2 = 0;
+    
+    int sum1 = 0;  
+    int sum2 = 0;  
     int Big = arr[0];
     int Small = arr[0];
-
-    for (int i = 1; i < n; i++) {
+    for (int i = 1; i < a; i++) {
         if (arr[i] > Big) {
             Big = arr[i];
         }
@@ -20,14 +20,13 @@ int main() {
             Small = arr[i];
         }
     }
-
-    for ( int i = 0 ; i < a ; i ++ ){
-        sum1 = sum1 + arr[i];
+    for (int i = 0; i < a; i++){
+        sum1 += arr[i];
     }
-    for ( int i = Small ; i < big ;i ++){
-        sum2 = sum2 + i;
+    for (int i = Small; i <= Big; i++){
+        sum2 += i;
     }
-
-    printf("%d", sum2 - sum1);
+    printf("%d", (sum2 - sum1));
+    
     return 0;
 }
