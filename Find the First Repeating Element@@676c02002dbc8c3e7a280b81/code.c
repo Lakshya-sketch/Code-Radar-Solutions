60d3263947f2cf9eb4f5abb8effca988d19a413e
Lakshya-sketch <134377132+Lakshya-sketch@users.noreply.github.com>
2025-02-07 19:09:32 +0530

    Solution to Find the First Repeating Element

--- Find the First Repeating Element@@676c02002dbc8c3e7a280b81/code.c
+++ Find the First Repeating Element@@676c02002dbc8c3e7a280b81/code.c
@@ -1,24 +1,21 @@
 #include <stdio.h>
-#include <limits.h> 
 
 int main() {
     int a;
     scanf("%d", &a);
     int arr[a];
-
     for (int i = 0; i < a; i++) {
-        scanf("%d ", &arr[i]);
+        scanf("%d", &arr[i]);
     }
-
-    int Largest = INT_MIN, product;
-
-    for (int i = 0; i < a - 1; i++) {
-        product = arr[i] * arr[i + 1]; 
-        if (product > Largest) {
-            Largest = product;
+    for (int i = 0; i < a; i++) {
+        for (int j = i + 1; j < a; j++) { 
+            if (arr[i] == arr[j]) {
+                printf("%d\n", arr[i]); 
+                return 0; 
+            }
         }
     }
+    printf("-1\n");
 
-    printf("%d\n", Largest);
     return 0;
 }
