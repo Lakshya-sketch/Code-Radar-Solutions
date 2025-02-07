d55ed57cb1c93dfd2deaee83c9fe16c4563527c9
Lakshya-sketch <134377132+Lakshya-sketch@users.noreply.github.com>
2025-02-07 21:03:20 +0530

    Solution to Create a Program to Store and Display Student Records

--- Create a Program to Store and Display Student Records@@6785070ed37d6039a711ebee/code.c
+++ Create a Program to Store and Display Student Records@@6785070ed37d6039a711ebee/code.c
@@ -6,14 +6,11 @@ void print(int Roll_Number, char Name[], int Marks) {
 }
 
 int main() {
-    char Name1[100], Name2[100];
-    int Roll_Number1, Marks1, Roll_Number2, Marks2;
-
-    printf("Enter details for student 1 (Roll_Number Name Marks): ");
-    print(Roll_Number1, Name1, Marks1);
+    char Name[100];
+    int Roll_Number, Marks;
+    printf(print(int Roll_Number,char Name[],int Marks));
+    printf(print(int Roll_Number,char Name[],int Marks));
     
-    printf("Enter details for student 2 (Roll_Number Name Marks): ");
-    print(Roll_Number2, Name2, Marks2);
 
     return 0;
 }
