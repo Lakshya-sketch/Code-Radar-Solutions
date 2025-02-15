95804e1e6f3b8b819595bb068b760a7308802867
Lakshya_Bakshi <134377132+Lakshya-sketch@users.noreply.github.com>
2025-02-15 10:23:35 +0530

    Solution to Convert Decimal to Binary

--- Convert Decimal to Binary@@67882a731b4392d8fc1577f5/code.c
+++ Convert Decimal to Binary@@67882a731b4392d8fc1577f5/code.c
@@ -4,6 +4,10 @@ void DecToBin(int n) {
     int bin[32];
     int i = 0;   
 
+    if( n == 0 ){
+        return "0";
+    }
+
     while (n > 0) {
         bin[i] = n % 2; 
         n = n / 2;      
