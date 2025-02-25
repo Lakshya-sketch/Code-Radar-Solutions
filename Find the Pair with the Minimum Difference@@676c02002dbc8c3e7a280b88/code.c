b8ba9e90f9dbcc96a4cf8d431477811d85611ace
Lakshya_Bakshi <134377132+Lakshya-sketch@users.noreply.github.com>
2025-02-25 21:37:42 +0530

    Solution to Find the Pair with the Minimum Difference

--- Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
+++ Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
@@ -1,47 +1,36 @@
 #include <stdio.h>
 #include <limits.h>
 #include <stdlib.h>
-#include <math.h>
 
-struct pair{
+struct pair {
     int differ;
-    int a,b;
+    int a, b;
 };
 
 int main() {
     int a;
     scanf("%d", &a);
 
-    struct pair diff[a];
-
     int arr[a];
     for (int i = 0; i < a; i++) {
         scanf("%d", &arr[i]);
     }
-    int difference,index;
+
+    struct pair diff;
     int min = INT_MAX; 
-    for( int i = 0 ; i < a ;i++){
-        if( i == 0){
-            difference = abs(arr[a-1] - arr[0]);
-            diff[i].differ = difference;
-            diff[i].a = arr[a-1];
-            diff[i].b = arr[0];
-        }else if( i == (a-1) ){
-            continue;
-        }
-        else{
-            difference = abs(arr[i+1] - arr[i]);
-            diff[i].differ = difference;
-            diff[i].a = arr[i];
-            diff[i].b = arr[i+1];
-        }
-        if(min > diff[i].differ){
-            min = diff[i].differ;
-            index = i;
+
+    for (int i = 0; i < a; i++) {
+        int next = (i + 1) % a; 
+        int difference = abs(arr[next] - arr[i]);
+
+        if (difference < min) {
+            min = difference;
+            diff.differ = difference;
+            diff.a = arr[i];
+            diff.b = arr[next];
         }
     }
-    printf("%d %d",diff[index].a,diff[index].b);
-    
 
+    printf("%d %d", diff.a, diff.b);
     return 0;
 }
