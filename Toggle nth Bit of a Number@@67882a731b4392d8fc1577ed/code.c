4d6c29e776d64ec4eba5be15aa2f110e57264d22
Lakshya_Bakshi <134377132+Lakshya-sketch@users.noreply.github.com>
2025-03-01 16:20:22 +0530

    Solution to Toggle nth Bit of a Number

--- Toggle nth Bit of a Number@@67882a731b4392d8fc1577ed/code.c
+++ Toggle nth Bit of a Number@@67882a731b4392d8fc1577ed/code.c
@@ -1,27 +1,27 @@
 #include <stdio.h>
 
-void DecToBin(int n) {
-    int bin[32]; 
-    int i = 0;  
+void DecToBin(int n, int bin[], int *size) {
+    int i = 0;
     while (n > 0) {
-        bin[i] = n % 2; 
-        n = n / 2;       
+        bin[i] = n % 2;
+        n = n / 2;
         i++;
     }
+    *size = i; 
 }
 
 int main() {
-    int n;
+    int n, posit
+    int size = 0; 
+
     scanf("%d %d", &n, &position);
-    DecToBin(n);
-    if (bin[positoin] == "0"){
-        bin[positoin] = 1;
+
+    DecToBin(n, bin, &size);
+    bin[position] = (bin[position] == 0) ? 1 : 0;
+    for (int j = size - 1; j >= 0; j--) {
+        printf("%d", bin[j]);
     }
-    else{
-        bin[position] = 0;
-    }
-    for(int j = i - 1; j >= 0 ; j--){
-        printf("%d",bin[j]);
-    }
+    printf("\n");
+
     return 0;
 }
